fd4f9e756574f1b5b968f46753c580af7d635f90
mahlet-16 <mahletamenu16@gmail.com>
2025-03-16 00:52:34 +0300

    Add files via upload
    advanced api

---
+++ advanced-api-project/api/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import filters
+from rest_framework import generics
+from rest_framework import viewsets
+
+# Create your views here.   
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class BookFilter(filters.FilterSet):
+    class Meta:
+        model = Book
+        fields = {
+            'title': ['icontains'],
+            'author': ['icontains'],
+            'publication_year': ['exact', 'gte', 'lte'],
+        }
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, filters.DjangoFilterBackend]
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
