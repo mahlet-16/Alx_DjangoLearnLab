b4fe020bfdfee39d296b76d1474964e885f5b82a
mahlet-16 <mahletamenu16@gmail.com>
2025-03-23 17:19:46 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -13,8 +13,7 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=300)
     content = models.TextField(max_length=800)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     tags = TaggableManager()
     
