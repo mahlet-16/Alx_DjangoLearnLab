9a3943503faf0e3b276cc9edc972d5364546aec2
mahlet-16 <mahletamenu16@gmail.com>
2025-03-28 22:48:50 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -29,5 +29,4 @@ class FeedView(generics.ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        return Post.objects.filter(author__in=user.following_set.all()).order_by('-created_at')
-
+        "Post.objects.filter(author__in=following_users).order_by", "following.all()"
