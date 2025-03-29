512f21e632b79718f5ac33e08cec58cb0795c441
mahlet-16 <mahletamenu16@gmail.com>
2025-03-29 14:12:36 +0300

    Delete social_media_api directory

--- social_media_api/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework.authtoken.views import ObtainAuthToken
-from .views import RegisterView, follow_user, unfollow_user
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', ObtainAuthToken.as_view(), name='login'),
-    path("follow/<int:user_id>/", follow_user, name="follow_user"),
-    path("unfollow/<int:user_id>/", unfollow_user, name="unfollow_user"),]
